e2b04c3e67a91d269d03d60a4a82d3a416d0c924
FrostoCosco <153654373+FrostoCosco@users.noreply.github.com>
2024-07-19 18:38:58 +0200

    Update UpdateChecker.py

--- UpdateChecker.py
+++ UpdateChecker.py
@@ -1,4 +1,16 @@
 # This file is used to automatically keep the game client up-to-date with newer releases of source ports and versions of ZVN.
 # - @Divided
 
+# Import libs.
+import os
+import platform
+
+# Create a clear function.
+def Clear():
+  if platform.os == "Windows":
+    os.system("cls")
+  else:
+    os.system("clear")
+
+# Run some test code.
 print("test!")
